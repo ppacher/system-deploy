c3bd1845b76a70674cad0109172e5876e1ceadaf
Patrick Pacher <p.pacher@cybertrap.com>
2020-07-05 11:49:50 +0200

    integration: add create-user use-case

--- integration/usecases/create-user/verify.sh
+++ integration/usecases/create-user/verify.sh
@@ -4,5 +4,6 @@ set -e
 [[ $(cat /etc/passwd | grep bar | grep zsh) != "" ]] || exit 1
 
 ls -lah /home/bar
+find . /home -name .oh-my-zsh
 [[ -d /home/bar/.oh-my-zsh ]] || exit 1
 [[ "$(cat /home/bar/.zshrc)" == "# example file" ]] || exit 1
